fe207494bf4bc2aeaaf6006d7a42fc0336161394
elmendezz <165435968+elmendezz@users.noreply.github.com>
2025-09-27 12:48:17 -0700

    update sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Nombre de la caché
+const CACHE_NAME = 'horario-1cv-cache-v1';
+
+// Archivos para cachear
+const urlsToCache = [
+  '/',
+  'index.html',
+  'horario.jpg',
+  'admin.html',
+  'index_beta.html'
+];
+
+// Evento de instalación: se abre la caché y se añaden los archivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Evento fetch: responde desde la caché si es posible
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Si el recurso está en la caché, lo devuelve. Si no, lo busca en la red.
+        return response || fetch(event.request);
+      })
+  );
+});
