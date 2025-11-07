35fcf6a9adf4d71abb5b8566484f3139fb61ab73
elmendezz <165435968+elmendezz@users.noreply.github.com>
2025-11-06 20:12:16 -0800

    Add recess and free periods to 1A-1F schedules
    Inserted scheduled recesses (20 and 10 minutes) and 'Libre' periods across all days in schedule-data-1A.js to schedule-data-1F.js for consistency and improved clarity in daily timetables.

--- schedule-data-1E.js
+++ schedule-data-1E.js
@@ -7,21 +7,30 @@ export const schedule = [
     [
         { time: [7, 0], name: "Ingles I", teacher: "RODRIGUEZ CHAVEZ MIGUEL ANGEL" },
         { time: [7, 50], name: "Humanidades I", teacher: "GONZALEZ FIGUEROA CARLOS MANUEL" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "La Materia y sus Interacciones", teacher: "VALDEZ ORTEGA DANIEL" },
+        { time: [9, 50], name: "Libre", teacher: "" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 },
         { time: [10, 50], name: "Pensamiento matemático I", teacher: "SANDOVAL ONTIVEROS HUGO" },
         { time: [11, 40], name: "Pensamiento matemático I", teacher: "SANDOVAL ONTIVEROS HUGO" }
     ],
     // Martes
     [
         { time: [7, 50], name: "Humanidades I", teacher: "GONZALEZ FIGUEROA CARLOS MANUEL" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "Pensamiento matemático I", teacher: "SANDOVAL ONTIVEROS HUGO" },
+        { time: [9, 50], name: "Libre", teacher: "" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 },
         { time: [10, 50], name: "Humanidades I", teacher: "GONZALEZ FIGUEROA CARLOS MANUEL" },
         { time: [11, 40], name: "Ciencias Sociales I", teacher: "CASTRO MENDIVIL ARIADNA" }
     ],
     // Miércoles
     [
         { time: [7, 50], name: "Cultura Digital I", teacher: "SANZ SALAZAR FERNANDO HUMBERTO" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "La Materia y sus Interacciones", teacher: "VALDEZ ORTEGA DANIEL" },
+        { time: [9, 50], name: "Libre", teacher: "" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 },
         { time: [10, 50], name: "Humanidades I", teacher: "GONZALEZ FIGUEROA CARLOS MANUEL" },
         { time: [11, 40], name: "Ciencias Sociales I", teacher: "CASTRO MENDIVIL ARIADNA" }
     ],
@@ -29,13 +38,18 @@ export const schedule = [
     [
         { time: [7, 0], name: "Ingles I", teacher: "RODRIGUEZ CHAVEZ MIGUEL ANGEL" },
         { time: [7, 50], name: "Ingles I", teacher: "RODRIGUEZ CHAVEZ MIGUEL ANGEL" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "Lengua y Comunicación I", teacher: "MONJARAS FONTES BERTHA ADRIANA" },
-        { time: [9, 50], name: "Lengua y Comunicación I", teacher: "MONJARAS FONTES BERTHA ADRIANA" }
+        { time: [9, 50], name: "Lengua y Comunicación I", teacher: "MONJARAS FONTES BERTHA ADRIANA" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 }
     ],
     // Viernes
     [
         { time: [7, 50], name: "Formación socioemocional I", teacher: "FLORES VELAZQUEZ MARIA LEONOR" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "Cultura Digital I", teacher: "SANZ SALAZAR FERNANDO HUMBERTO" },
+        { time: [9, 50], name: "Libre", teacher: "" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 },
         { time: [10, 50], name: "Lengua y Comunicación I", teacher: "MONJARAS FONTES BERTHA ADRIANA" }
     ]
 ];
--- schedule-data-1F.js
+++ schedule-data-1F.js
@@ -7,35 +7,47 @@ export const schedule = [
     [
         { time: [7, 0], name: "Humanidades I", teacher: "GONZALEZ FIGUEROA CARLOS MANUEL" },
         { time: [7, 50], name: "Ingles I", teacher: "FLORES ORTIZ GRECIA CAROLINA" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "Cultura Digital I", teacher: "SANZ SALAZAR FERNANDO HUMBERTO" },
-        { time: [9, 50], name: "Cultura Digital I", teacher: "SANZ SALAZAR FERNANDO HUMBERTO" }
+        { time: [9, 50], name: "Cultura Digital I", teacher: "SANZ SALAZAR FERNANDO HUMBERTO" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 }
     ],
     // Martes
     [
         { time: [7, 0], name: "Cultura Digital I", teacher: "SANZ SALAZAR FERNANDO HUMBERTO" },
         { time: [7, 50], name: "Lengua y Comunicación I", teacher: "MACHADO BERNAL HERIBERTO" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "Ingles I", teacher: "FLORES ORTIZ GRECIA CAROLINA" },
         { time: [9, 50], name: "Humanidades I", teacher: "GONZALEZ FIGUEROA CARLOS MANUEL" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 },
         { time: [11, 40], name: "Pensamiento matemático I", teacher: "SANDOVAL ONTIVEROS HUGO" }
     ],
     // Miércoles
     [
         { time: [7, 0], name: "Humanidades I", teacher: "GONZALEZ FIGUEROA CARLOS MANUEL" },
         { time: [7, 50], name: "Ingles I", teacher: "FLORES ORTIZ GRECIA CAROLINA" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "Formación socioemocional I", teacher: "FLORES VELAZQUEZ MARIA LEONOR" },
         { time: [9, 50], name: "Ciencias Sociales I", teacher: "CASTRO MENDIVIL ARIADNA" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 },
         { time: [11, 40], name: "La Materia y sus Interacciones", teacher: "VALDEZ ORTEGA DANIEL" }
     ],
     // Jueves
     [
+        { time: [7, 0], name: "Libre", teacher: "" },
+        { time: [7, 50], name: "Libre", teacher: "" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "Humanidades I", teacher: "GONZALEZ FIGUEROA CARLOS MANUEL" },
-        { time: [9, 50], name: "Ciencias Sociales I", teacher: "CASTRO MENDIVIL ARIADNA" }
+        { time: [9, 50], name: "Ciencias Sociales I", teacher: "CASTRO MENDIVIL ARIADNA" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 }
     ],
     // Viernes
     [
         { time: [7, 0], name: "La Materia y sus Interacciones", teacher: "VALDEZ ORTEGA DANIEL" },
         { time: [7, 50], name: "Pensamiento matemático I", teacher: "SANDOVAL ONTIVEROS HUGO" },
+        { time: [8, 40], name: "Receso", teacher: "Pausa de 20 Minutos", duration: 20 },
         { time: [9, 0], name: "Lengua y Comunicación I", teacher: "MACHADO BERNAL HERIBERTO" },
-        { time: [9, 50], name: "Lengua y Comunicación I", teacher: "MACHADO BERNAL HERIBERTO" }
+        { time: [9, 50], name: "Lengua y Comunicación I", teacher: "MACHADO BERNAL HERIBERTO" },
+        { time: [10, 40], name: "Receso", teacher: "Pausa de 10 Minutos", duration: 10 }
     ]
 ];
